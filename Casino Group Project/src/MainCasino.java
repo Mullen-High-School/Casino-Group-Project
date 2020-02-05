ce9c00f5ddd8be16bb43c49b7aa5792f4e6c3776
Mike McGuire <MoonshotMike@gmail.com>
2020-02-04 18:51:17 -0700

    initial commit

---
+++ Casino Group Project/src/MainCasino.java
@@ -0,0 +1,18 @@
+
+public class MainCasino
+	{
+	public static void main(String[] args)
+		{
+		welcome();
+		presentGameMenu();
+		}
+	public static void welcome()
+		{
+		
+		}
+	
+	public static void presentGameMenu()
+		{
+		System.out.println("Which game would you like to play?");
+		}
+	}
